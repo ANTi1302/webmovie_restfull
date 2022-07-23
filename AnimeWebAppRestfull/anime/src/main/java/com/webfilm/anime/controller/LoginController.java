99bf698976df60dca2b92eb8d116a59ff3cb9424
ANTi1302 <porkoe3443334@gmail.com>
2022-07-23 17:55:05 +0700

    ok

--- AnimeWebAppRestfull/anime/src/main/java/com/webfilm/anime/controller/LoginController.java
+++ AnimeWebAppRestfull/anime/src/main/java/com/webfilm/anime/controller/LoginController.java
@@ -1,13 +1,17 @@
 package com.webfilm.anime.controller;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.webfilm.anime.entity.Users;
 import com.webfilm.anime.service.CountryService;
 import com.webfilm.anime.service.GenresService;
 import com.webfilm.anime.service.MenuService;
@@ -17,9 +21,15 @@ import com.webfilm.anime.service.SeriesService;
 public class LoginController extends BaseController{
 
 	@RequestMapping({"/login","/admin"})
-	public ModelAndView showformlogin(HttpServletResponse response, HttpServletRequest request) {
-
-		modelAndView.setViewName("customer/login");
+	public ModelAndView showformlogin(HttpServletResponse response, HttpServletRequest request) throws IOException {
+		HttpSession session = request.getSession();
+		Users username = (Users) session.getAttribute("acc");
+		if (username !=null) {
+			response.sendRedirect("/");
+		}else {
+			modelAndView.setViewName("customer/login");
+		}
+		
 		return modelAndView;
 	}
 }
